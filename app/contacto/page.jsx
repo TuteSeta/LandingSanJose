3c3575bd0c6a2e02110c124d0ef7d733c846e539
Matias Zarandon <zarandonmatias@gmail.com>
2025-10-13 13:07:43 -0300

    Noticias comenzado

--- app/contacto/page.jsx
+++
@@ -1,8 +0,0 @@
-import Contacto from "../components/contacto/contacto.jsx";
-export default function Home() {
-  return (
-    <div className="min-h-dvh bg-[#F9FAFB]">
-      <Contacto></Contacto>
-    </div>
-  );
-}
